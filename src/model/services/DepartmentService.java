c8bd03c005883d5ecce3a1bf75dfbfd599a1f24d
Viviane Confortin <vivianeconfortin01@gmail.com>
2020-09-28 09:29:32 -0300

    Remove department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -7,19 +7,22 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
-	
+
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
-	
-	public List<Department> findAll(){
+
+	public List<Department> findAll() {
 		return dao.findAll();
 	}
-	
+
 	public void saveOrUpdate(Department obj) {
-		if(obj.getId() == null) {
+		if (obj.getId() == null) {
 			dao.insert(obj);
-		}
-		else {
+		} else {
 			dao.update(obj);
 		}
+	}
+
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
 	}
 }
